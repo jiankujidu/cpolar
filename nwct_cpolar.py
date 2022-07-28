bed10ee942452c38680addf65a1a9dde48678fa7
艰苦极度 <105758941+jiankujidu@users.noreply.github.com>
2022-07-29 05:41:14 +0800

    Add files via upload

--- nwct_cpolar.py
+++ nwct_cpolar.py
@@ -1,18 +1,21 @@
 # -*- coding: UTF-8 -*-
-# Version: v1.0
+# Version: v1.1
 # Created by lstcml on 2022/07/21
 # 建议定时10分钟：*/10 * * * *
 
 '''
+使用说明：
+1、https://i.cpolar.com/m/4LUd/注册登录后获取authtoken；
+2、新增变量qlnwct_authtoken，值为你账户的authtoken，运行脚本
+
+更新记录：
+V1.1
+1、开放推送，仅支持PushPlus推送，每次触发启动穿透会推送一次地址
+'''
+'''
 cron： */10 * * * * *
 new Env（'cpolar内网穿透'）;
 '''
-
-'''
-使用说明：
-1、打开https://www.cpolar.com/注册登录后获取authtoken；
-2、新增变量qlnwct_authtoken，值为你账户的authtoken，运行脚本
-''' 
 
 import os
 import re
@@ -28,15 +31,15 @@ app_path = os.path.join(path, "cpolar")
 commond = "python3 " + os.path.join(path, "cpolar.py") + " &"
 
 def update():
-    print("公众号：一起瞎折腾\nQQ交流群:641307462\n当前运行的脚本版本：" + str(version))
+    print("当前运行的脚本版本：" + str(version))
     try:
-        r1 = requests.get("https://ghproxy.com/https://raw.githubusercontent.com/jiankujidu/cpolar/main/nwct_cpolar.py").text
+        r1 = requests.get("https://gitee.com/lstcml/qinglongscripts/raw/master/nwct_cpolar.py").text
         r2 = re.findall(re.compile("version = \d.\d"), r1)[0].split("=")[1].strip()
         if float(r2) > version:
-            print("公众号：一起瞎折腾\nQQ交流群:641307462\n发现新版本：" + r2)
-            print("公众号：一起瞎折腾\nQQ交流群:641307462\n正在自动更新脚本...")
+            print("发现新版本：" + r2)
+            print("正在自动更新脚本...")
             os.system("killall cpolar")
-            os.system("ql raw https://ghproxy.com/https://raw.githubusercontent.com/jiankujidu/cpolar/main/nwct_cpolar.py &")
+            os.system("ql raw https://gitee.com/lstcml/qinglongscripts/raw/master/nwct_cpolar.py &")
     except:
         pass
 
@@ -44,7 +47,7 @@ def update():
 # 下载主程序
 def download_cpolar():
     if not os.path.exists("cpolar.py"):
-        res = requests.get("https://ghproxy.com/https://raw.githubusercontent.com/jiankujidu/cpolar/main/cpolar.py")
+        res = requests.get("https://gitee.com/lstcml/qinglongscripts/raw/master/cpolar.py")
         with open("cpolar.py", "wb") as f:
             f.write(res.content)
     if not os.path.exists("cpolar"):
@@ -62,12 +65,11 @@ def get_url():
         reg = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
         for i in re.findall(reg, log_content):
             if 'cpolar' in i:
-                print("公众号：一起瞎折腾\nQQ交流群:641307462\n获取穿透链接成功...")
-                print(i)
+                print("获取穿透链接成功...")
                 return i.replace('\\', '')
                 break
     except:
-        return "https://ghproxy.com/https://raw.githubusercontent.com/jiankujidu/cpolar"
+        return "https://gitee.com/lstcml/qinglongscripts"
 
 # 进程守护
 def process_daemon():
@@ -90,17 +92,17 @@ def start_nwct():
         os.system("rm -rf " + log_path)
         os.system("mkdir -p " + log_path)
         os.system("killall cpolar >/dev/null 2>&1")
-        print("公众号：一起瞎折腾\nQQ交流群:641307462\n正在启动内网穿透...")
+        print("正在启动内网穿透...")
         os.system(commond)
         sleep(10)
         if process_daemon():
             if load_send():
-                print("公众号：一起瞎折腾\nQQ交流群:641307462\n启动内网穿透成功！\n青龙面板：%s" % qlurl)
-            #  send("公众号：一起瞎折腾\nQQ交流群:641307462\n内网穿透通知", "青龙面板访问地址：" + qlurl)
+                print("启动内网穿透成功！\n青龙面板：%s" % qlurl)
+                send("内网穿透通知", "青龙面板访问地址：" + qlurl)
         else:
-            print("公众号：一起瞎折腾\nQQ交流群:641307462\n启动内网穿透失败...")
+            print("启动内网穿透失败...")
     else:
-        print("公众号：一起瞎折腾\nQQ交流群:641307462\n穿透程序已在运行...\n青龙面板：%s" % qlurl)
+        print("穿透程序已在运行...\n青龙面板：%s" % qlurl)
 
 
 # 推送
@@ -110,7 +112,7 @@ def load_send():
     sys.path.append(cur_path)
     sendNotifPath = cur_path + "/sendNotify.py"
     if not os.path.exists(sendNotifPath):
-        res = requests.get("https://ghproxy.com/https://raw.githubusercontent.com/jiankujidu/cpolar/main/sendNotify.py")
+        res = requests.get("https://gitee.com/lstcml/qinglongscripts/raw/master/sendNotify.py")
         with open(sendNotifPath, "wb") as f:
             f.write(res.content)
 
@@ -123,7 +125,7 @@ def load_send():
 
 
 if __name__ == '__main__':
-    version = 1.0
+    version = 1.1
     try:
         authtoken = os.environ['qlnwct_authtoken']
     except:
@@ -140,8 +142,8 @@ if __name__ == '__main__':
     if check_update == "true":
         update()
     else:
-        print("公众号：一起瞎折腾\nQQ交流群:641307462\n变量qlnwctupdate未设置，脚本自动更新未开启！")
+        print("变量qlnwctupdate未设置，脚本自动更新未开启！")
     if len(authtoken ) < 1:
-        print("公众号：一起瞎折腾\nQQ交流群:641307462\n请新增变量qlnwct_authtoken！")
+        print("请新增变量qlnwct_authtoken！")
     else:
         download_cpolar()
